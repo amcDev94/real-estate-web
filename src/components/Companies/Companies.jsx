d618eedeeee2a085d701977491e3f35d90c71975
Angel Martínez <angelmc.developer@gmail.com>
2023-05-13 18:36:22 +0200

    Add functionalities

---
+++ src/components/Companies/Companies.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Companies.css";
+
+const Companies = () => {
+  return (
+    <section className="c-wrapper">
+      <div className="paddings innerWidth flexCenter c-container">
+        <img src="../../../public/prologis.png" alt="prologis" />
+        <img src="../../../public/tower.png" alt="tower" />
+        <img src="../../../public/equinix.png" alt="equinix" />
+        <img src="../../../public/realty.png" alt="realty" />
+      </div>
+    </section>
+  );
+};
+
+export default Companies;
